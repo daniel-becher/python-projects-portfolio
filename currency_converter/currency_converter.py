a5d2481acbf0833863b4ad773980aa8fd6c5d378
Daniel Becher <131185354+daniel-becher@users.noreply.github.com>
2026-04-12 23:34:09 +0200

    Refactor currency converter

--- currency_converter/currency_converter.py
+++ currency_converter/currency_converter.py
@@ -1,7 +1,6 @@
-import tkinter as tk
 import requests
+import tkinter as tk
 
-# Fetching live exchange rates from the Czech National Bank
 url = "https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt"
 
 response = requests.get(url)
@@ -9,48 +8,60 @@ response = requests.get(url)
 if response.status_code == 200:
     data = response.text
 else:
-    print("Error during data downloading.")
-
-
+    print("Error during downloading data.")
 
 lines = data.split("\n")
 
-found = False
+def get_exchange_rate():
+    target_currency = currency_entry.get().upper()
 
-for line in lines:
-    parts = line.split("|")
-    if "EUR" in line and len(parts) > 4:
-        rate =  float(parts[4].replace(",", "."))
-        found = True
+    for line in lines:
+        data_columns = line.split("|")
         
-if not found:
-    print ("Currency not found.")
+        if len(data_columns) > 4 and target_currency == data_columns[3]:
+            exchange_rate = float(data_columns[4].replace(",", "."))
+            return exchange_rate, target_currency
+            
+    return None, None
+        
 
+def conversion():
+    input_value = amount_entry.get()
 
-def calc():
-    amount = entry.get()
-    
+    exchange_rate, code = get_exchange_rate()
+
+    if exchange_rate is None:
+        result_label.config(text="Invalid currency code!", fg="red")
+        return
+
     try:
-        result = round(float(amount) / rate, 2)
-        label.config(text=f"Result: {result} EUR", fg="green")
-        entry.delete(0, tk.END)
+        result = round(float(input_value) / exchange_rate, 2)
+        result_label.config(text=f"Result: {input_value} CZK = {result} {code}.", fg="green")
+        amount_entry.delete(0, tk.END)
     except ValueError:
-        label.config(text="Please enter a number!", fg="red")
-    
+        result_label.config(text="Enter a number.", fg="red")
+
 
 root = tk.Tk()
-root.title("Currency converter")
-root.geometry("300x200")
+root.title("Money converter")
+root.geometry("300x250")
 
-tk.Label(root, text="How much CZK do you want to convert?").pack(pady=10)
+tk.Label(root, text="How much CZK do you want to convert?: ").pack(pady=10)
 
-entry = tk.Entry(root)
-entry.pack(pady=10)
+amount_entry = tk.Entry(root)
+amount_entry.pack(pady=10)
 
-button = tk.Button(root, text="Convert", command=calc)
-button.pack()
+label_2 = tk.Label(root, text="What currency do you want to exchange to?: ")
+label_2.pack(pady=10)
 
-label = tk.Label(root, text="Nothing yet...", font=("Verdana", 12))
-label.pack(pady=10)
+currency_entry = tk.Entry(root)
+currency_entry.pack(pady=10)
+
+convert_button = tk.Button(root, text="Convert", command=conversion)
+convert_button.pack()
+
+result_label = tk.Label(root, text="Nothing yet...")
+result_label.pack()
+
 
 root.mainloop()
